774f2ea674bbe24c9fbb03b930497c4b82cd5f0c
unknown <kumarayush957680@gmail.com>
2023-09-02 22:16:04 +0530

    first commit

---
+++ app/services/adMobConfig.js
@@ -0,0 +1,19 @@
+export const admobConfig = {
+  admobEnabled: false,
+  admobBannerId: {
+    android: "ca-app-pub-5451733485152705/3212565001",
+    iOS: "ca-app-pub-5451733485152705/2654980046",
+  },
+  listAdType: "largeBanner",
+  detailScreenAdType: "largeBanner",
+  // Banner Ads
+  enableBannerInHomeScreen: false,
+  enableBannerInSearchScreen: false,
+  enableBannerInSingleListing: false,
+  enableBannerInSettingsScreen: false,
+  enableBannerInSellFasterScreenTop: false,
+  enableBannerInAllStoresTop: false,
+  enableInStoreDetail: false,
+  enableBannerInMyListings: false,
+  enableBannerInFavorites: false,
+};
---
+++ app/services/socialLoginConfig.js
@@ -0,0 +1,7 @@
+export const socialConfig = {
+  enabled: false,
+  socialPlatforms: [],
+  google: {
+    webClientId: "86796976-ji87hj0y8h76h87hu65g7h.apps.googleusercontent.com",
+  },
+};
